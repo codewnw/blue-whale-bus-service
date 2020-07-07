6de4ffe58194a6bf1d1e490d66111bbe94368a8c
gitk21 <48712101+gitk21@users.noreply.github.com>
2020-07-07 13:35:20 +0530

    Logging Changes (#4)
    * Backend Changes - Forgot Password
    
    * Default Error Handler and Modified File Name
    
    * Changed as per Review Comments
    
    * -Logging Changes - SLF4J/Log4J
    
    -Directory Structure Changes
    
    -CustomException - UserNotFoundException
    
    Co-authored-by: kaustu21 <48712101+kaustu21@users.noreply.github.com>

--- src/com/bluewhale/bus/servlet/Signup.java
+++ src/com/bluewhale/bus/servlet/Signup.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.bluewhale.bus.model.Login;
 import com.bluewhale.bus.model.Password;
 import com.bluewhale.bus.model.User;
@@ -19,6 +22,8 @@ import com.bluewhale.bus.service.UserServiceImpl;
 
 @WebServlet("/signup")
 public class Signup extends HttpServlet {
+
+	private static final Logger logger = LoggerFactory.getLogger(Signup.class);
 	private static final long serialVersionUID = 1L;
 
 	private MailService mailService;
@@ -35,11 +40,17 @@ public class Signup extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		logger.debug("Inside Class :" + this.getClass().getSimpleName());
+
 		response.sendRedirect("signup.jsp");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		logger.debug("Inside Class :" + this.getClass().getSimpleName());
+
 		String username = request.getParameter("email");
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
